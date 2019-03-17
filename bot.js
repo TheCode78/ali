44d1694092ef675b7bdc74c9328017428a9b3007
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-03-17 15:53:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -473,12 +473,6 @@ client.on('message', function(msg) {
 
 
 client.on('ready', () => {
-   console.log(`~~~~~~~~~~~~~~~~~`);
-   console.log(`Logging into Discord`);
-   console.log(`~~~~~~~~~~~~~~~~~~~~~`);
-   console.log(`on  ${client.guilds.size} Servers `);
-   console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
-   console.log(`Logged in as ${client.user.tag}!`);
    client.user.setStatus("online")
 });
  
