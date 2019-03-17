8c069d310906b0c2858c78a9db8c76ec461e17e2
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-03-17 15:13:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -479,8 +479,7 @@ client.on('ready', () => {
    console.log(`on  ${client.guilds.size} Servers `);
    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
    console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setGame(`^help | ^inv`)
-   client.user.setStatus("dnd")
+   client.user.setStatus("online")
 });
  
 
