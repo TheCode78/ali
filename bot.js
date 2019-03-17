98afe2bb69448a9eccbf51db51c98814cb948b9c
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-03-17 14:36:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,7 @@ client.on('message', message => {
 
   if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
   if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if(!reason7) return message.reply ("**اكتب سبب الطرد**");
   if (!message.guild.member(user)
   .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
 
@@ -169,17 +169,6 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-    var prefix = "^"
-    if (message.content === prefix + 'createT') {
-        if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
-                if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
-         message.guild.createChannel('اسم الروم الكتابي', 'text')
-         message.guild.createChannel('اسم الروم الصوتي', 'voice')
-    
-    message.channel.sendMessage('**Text Channel Was Succsesfluy Created**')
-    }
-    });
 
 
 
@@ -189,63 +178,15 @@ client.on('message', message => {
 
 
 
-client.on("message", message => {
-    const prefix = "^"
-              
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "image"){ 
-          const embed = new Discord.RichEmbed()
-  
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
-
-   message.channel.send({embed});
-      }
-  });
 
 
 
 
 
-client.on('message', message => {
-    var prefix = "^"
-     let command = message.content.split(" ")[0];
-   command = command.slice(prefix.length);
- 
-   let args = message.content.split(" ").slice(1);
+
+
  
  
- if(command == "draw") {
-     var Canvas = require('canvas')
-   , Image = new Canvas.Image
-   , canvas = new Canvas(450, 170)
-   , ctx = canvas.getContext('2d');
-   ctx.font = '30px Impact';
-   let args = message.content.split(" ").slice(1);
-   
- Image.src = canvas.toBuffer();
- 
-     console.log(Image);
- ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
- ctx.fillText(args.join("  "),110, 70);
- 
- 
- ctx.beginPath();
- ctx.lineTo(50, 102);
- ctx.stroke();
- 
- message.channel.sendFile(canvas.toBuffer());
- }
- 
- });
-
-
-
 
 client.on('message', message => {
     if (message.content.startsWith("^hack")) {
@@ -484,35 +425,7 @@ client.on('message', message => {
 
 
  
-var prefix = "^";
- 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
- 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
- 
-  let args = message.content.split(" ").slice(1);
- 
-// -say
-  if (command === "say") {
-          message.delete()
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-  }
- 
- 
- 
-if (command == "embed") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor(0x23b2d6)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
- 
- 
-});
+
  
 
 
@@ -565,26 +478,6 @@ client.on('message', function(msg) {
 
 
 
-client.on("message", msg => {
-           var prefix = "^";
-  if(msg.content.startsWith (prefix + "id")) {
-    if(!msg.channel.guild) return msg.reply('**:x: This command for servers **');        
-      const embed = new Discord.RichEmbed();
-  embed.addField(":cloud_tornado:  name", `__**[ ${msg.author.username}#${msg.author.discriminator} ]**__`, true)
-          .addField(":id:  id", `__**[ ${msg.author.id} ]**__`, true)
-          .setColor("RANDOM")
-          .setFooter(msg.author.username , msg.author.avatarURL)
-          .setThumbnail(`${msg.author.avatarURL}`)
-          .setTimestamp()
-          .setURL(`${msg.author.avatarURL}`)
-          .addField(':spy:  status', `__**[ ${msg.author.presence.status.toUpperCase()} ]**__`, true)
-          .addField(':satellite_orbital:   playing', `__**[ ${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name} ]**__`, true)
-          .addField(':military_medal:  role', `__**[ ${msg.member.roles.filter(r => r.name).size} ]**__`, true)
-          .addField(':robot:  bot?  ', `__**[ ${msg.author.bot.toString().toUpperCase()} ]**__`, true);
-      msg.channel.send({embed: embed})
-  }
-});
- 
  
 
 
@@ -592,20 +485,7 @@ client.on("message", msg => {
 
 
 
-client.on('message', message => {
-  if(message.content.startsWith ("^help")) {
-      if(!message.channel.guild) return;
-    let embed = new Discord.RichEmbed()
-    .setColor('#044812')
-          .setThumbnail(message.author.avatarURL)
-      .setFooter(message.author.username, message.author.avatarURL)
- 
-    .setDescription(`**[ تــــــــم الارســــال بالخـــــــاص ]** `)
-        message.channel.send()
- 
-message.channel.sendEmbed(embed)
-}
-});
+
 
 
 
@@ -630,19 +510,7 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-        if (message.content === "-inv") {
-            if(!message.channel.guild) return;
-        let embed = new Discord.RichEmbed()
-        .setAuthor(`-#| ${message.author.username} |#-`, message.author.avatarURL)      
-        .setTitle(`:small_orange_diamond:Click Here.. !`)
-        .setURL(`https://discordapp.com/oauth2/authorize?client_id=538678456125227019&permissions=8&scope=bot`)
-        .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")
-        .addField(':small_blue_diamond:Byـ:', "<@" + message.author.id + ">")        
-     message.channel.sendEmbed(embed);
-       }
-   });
- 
+
 
 
 
@@ -657,23 +525,14 @@ client.on('message', message => {
 client.on('message', message => {
      if (message.content === "^help") {
 message.author.send(`
-         **general**
-^say - يكرر الكلام
-^id معلوماتك
-^ping سرعة بوت
-^link رابط سيرفر في خاص
-^draw رسم
-^hack تهكير اي حساب عضو
       **Adminastor**
 ^bc برودكاست
 ^kick طرد مع سبب
 ^ban باند مع سبب
-^unmuch تقفيل شات
-^much فتح شات
 ^image صورة سيرفر
 ^mute اعطاء العضو ميوت
 ^unmute الغاء الميوت من العضو
-^giveawayيسوي قيف اواي
+^giveaway يسوي قيف اواي
 
 
 
@@ -784,14 +643,6 @@ message.reply(`coming-soon`)
 
 
 client.on('message', message => {
-     if (message.content === "^inv") {
-message.author.send(`https://discordapp.com/api/oauth2/authorize?client_id=538678456125227019&permissions=8&scope=bot`)
-      }
- });
-
-
-
-
 
 
 
@@ -805,7 +656,7 @@ var prefix = "^";
  
     if (message.author.id === client.user.id) return;
     if (message.guild) {
-   let embed = new Discord.RichEmbed()
+   let embed = new Dord.RichEmbed()
     let args = message.content.split(' ').slice(1).join(' ');
 if(message.content.split(' ')[0] == prefix + 'bc') {
     if (!args[1]) {
@@ -887,39 +738,6 @@ message.channel.send(`${user} has **${inviteCount}** invites.`);
 
 
 
-
-
-
-var prefix = "^";
-
-client.on('message', message => {
-
-    if(message.content === prefix + "unmuch") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-           message.channel.overwritePermissions(message.guild.id, {
-         SEND_MESSAGES: false
-
-           }).then(() => {
-               message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
-           });
-             }
- if(message.content === prefix + "unmutechannel") {
-                     if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-           message.channel.overwritePermissions(message.guild.id, {
-         SEND_MESSAGES: true
-
-           }).then(() => {
-               message.reply("**__تم فتح الشات__:white_check_mark:**")
-           });
-             }
-             
-      
-    
-});
 
 
 
